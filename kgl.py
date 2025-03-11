d4efe58fd61f41038daa18f76178ce2b64526378
faizaijja <128844834+faizaijja@users.noreply.github.com>
2025-03-11 10:24:50 +0200

    Update kgl.py

--- kgl.py
+++ kgl.py
@@ -2,7 +2,7 @@ import time
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
+import plotly.graph_objects
 import plotly.express as px
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
